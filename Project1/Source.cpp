f55b9a2f48a0a3bc8a8be03cbcf6e5e3420ecd39
Богдан Хміль <bogdan.khmil2@gmail.com>
2023-08-29 20:25:50 +0300

    Esc fixed

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,37 +1,38 @@
-#include <windows.h> 
-#include <stdio.h> 
- 
-// Function for handling keyboard messages 
-LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) { 
-    if (nCode == HC_ACTION) { 
-        KBDLLHOOKSTRUCT* kbdStruct = (KBDLLHOOKSTRUCT*)lParam; 
-        int keycode = kbdStruct->vkCode; 
-        printf("Key code: %d\n", keycode); 
-    } 
-    return CallNextHookEx(NULL, nCode, wParam, lParam); 
-} 
- 
-int main() { 
-    // Set a hook to handle keyboard messages 
-    HHOOK hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, GetModuleHandle(NULL), 0); 
-    if (hHook == NULL) { 
-        printf("SetWindowsHookEx failed with error code %d\n", GetLastError()); 
-        return 1; 
-    } 
- 
-    // Message loop to keep the application running 
-    MSG msg; 
-    while (GetMessage(&msg, NULL, 0, 0)) { 
-        TranslateMessage(&msg); 
-        DispatchMessage(&msg); 
- 
-        // Check for the ESC key 
-        if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) { 
-            break; 
-        } 
-    } 
- 
-    UnhookWindowsHookEx(hHook); 
- 
-    return 0; 
+#include <windows.h>
+#include <stdio.h>
+
+HHOOK hHook = NULL; // Declare the hook handle globally
+
+// Function for handling keyboard messages
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode == HC_ACTION) {
+        KBDLLHOOKSTRUCT* kbdStruct = (KBDLLHOOKSTRUCT*)lParam;
+        int keycode = kbdStruct->vkCode;
+        printf("Key code: %d\n", keycode);
+
+        // Check for the ESC key and exit if pressed
+        if (keycode == VK_ESCAPE) {
+            UnhookWindowsHookEx(hHook);
+            PostQuitMessage(0); // This will signal the message loop to exit
+        }
+    }
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+int main() {
+    // Set a hook to handle keyboard messages
+    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, GetModuleHandle(NULL), 0);
+    if (hHook == NULL) {
+        printf("SetWindowsHookEx failed with error code %d\n", GetLastError());
+        return 1;
+    }
+
+    // Message loop to keep the application running
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
 }
